102af4a92376cdd4dc73aa91a5ee1ecbc88ba05f
Aleksandr Belousov <aleksandr.belousov@swisschain.io>
2020-08-27 17:48:52 +0400

    Added StreamItemCollection abstractions for streaming

---
+++ src/Swisschain.Sdk.Server/IStreamItem.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Swisschain.Extensions.Grpc.Abstractions
+{
+    public interface IStreamItem<TId> where TId : IComparable<TId>, IComparable
+    {
+        TId StreamItemId { get; }
+    }
+}
+
---
+++ src/Swisschain.Sdk.Server/IStreamItemCollection.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Swisschain.Extensions.Grpc.Abstractions
+{
+    public interface IStreamItemCollection<out TStreamItem, TId> 
+        where TId : IComparable<TId>, IComparable
+        where TStreamItem : IStreamItem<TId>
+    {
+        IReadOnlyCollection<TStreamItem> StreamItems { get; }
+    }
+}
