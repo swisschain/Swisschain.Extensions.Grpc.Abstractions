7e443ed96a306a8c131e9d9416dc4a181288d471
Andrey Volkov <avolkov.dev.10x@gmail.com>
2021-10-13 12:54:21 +0300

    SIR-2483 Do not apply deadline to streaming calls

--- src/Swisschain.Extensions.Grpc.Abstractions/ServiceDeadline/GlobalDeadlineInterceptor.cs
+++ src/Swisschain.Extensions.Grpc.Abstractions/ServiceDeadline/GlobalDeadlineInterceptor.cs
@@ -15,63 +15,6 @@ namespace Swisschain.Extensions.Grpc.Abstractions.ServiceDeadline
         {
             _options = options;
         }
-        
-        public override AsyncClientStreamingCall<TRequest, TResponse> AsyncClientStreamingCall<TRequest, TResponse>(ClientInterceptorContext<TRequest, TResponse> context, AsyncClientStreamingCallContinuation<TRequest, TResponse> continuation)
-        {
-            var call = continuation(ConfigureContext(context, out var cts));
-            if (cts == null)
-            {
-                return call;
-            }
-            else
-            {
-                return new AsyncClientStreamingCall<TRequest, TResponse>(
-                    requestStream: call.RequestStream,
-                    responseAsync: call.ResponseAsync,
-                    responseHeadersAsync: ClientStreamingCallbacks<TRequest, TResponse>.GetResponseHeadersAsync,
-                    getStatusFunc: ClientStreamingCallbacks<TRequest, TResponse>.GetStatus,
-                    getTrailersFunc: ClientStreamingCallbacks<TRequest, TResponse>.GetTrailers,
-                    disposeAction: ClientStreamingCallbacks<TRequest, TResponse>.Dispose,
-                    CreateContextState(call, cts));
-            }
-        }
-
-        public override AsyncDuplexStreamingCall<TRequest, TResponse> AsyncDuplexStreamingCall<TRequest, TResponse>(ClientInterceptorContext<TRequest, TResponse> context, AsyncDuplexStreamingCallContinuation<TRequest, TResponse> continuation)
-        {
-            var call = continuation(ConfigureContext(context, out var cts));
-            if (cts == null)
-            {
-                return call;
-            }
-
-            return new AsyncDuplexStreamingCall<TRequest, TResponse>(
-                requestStream: call.RequestStream,
-                responseStream: call.ResponseStream,
-                responseHeadersAsync: DuplexStreamingCallbacks<TRequest, TResponse>.GetResponseHeadersAsync,
-                getStatusFunc: DuplexStreamingCallbacks<TRequest, TResponse>.GetStatus,
-                getTrailersFunc: DuplexStreamingCallbacks<TRequest, TResponse>.GetTrailers,
-                disposeAction: DuplexStreamingCallbacks<TRequest, TResponse>.Dispose,
-                CreateContextState(call, cts));
-        }
-
-        public override AsyncServerStreamingCall<TResponse> AsyncServerStreamingCall<TRequest, TResponse>(TRequest request, ClientInterceptorContext<TRequest, TResponse> context, AsyncServerStreamingCallContinuation<TRequest, TResponse> continuation)
-        {
-            var call = continuation(request, ConfigureContext(context, out var cts));
-            if (cts == null)
-            {
-                return call;
-            }
-            else
-            {
-                return new AsyncServerStreamingCall<TResponse>(
-                    responseStream: call.ResponseStream,
-                    responseHeadersAsync: ServerStreamingCallbacks<TResponse>.GetResponseHeadersAsync,
-                    getStatusFunc: ServerStreamingCallbacks<TResponse>.GetStatus,
-                    getTrailersFunc: ServerStreamingCallbacks<TResponse>.GetTrailers,
-                    disposeAction: ServerStreamingCallbacks<TResponse>.Dispose,
-                    CreateContextState(call, cts));
-            }
-        }
 
         public override AsyncUnaryCall<TResponse> AsyncUnaryCall<TRequest, TResponse>(TRequest request, ClientInterceptorContext<TRequest, TResponse> context, AsyncUnaryCallContinuation<TRequest, TResponse> continuation)
         {
@@ -151,35 +94,6 @@ namespace Swisschain.Extensions.Grpc.Abstractions.ServiceDeadline
             internal static readonly Func<object, Status> GetStatus = state => ((ContextState<AsyncUnaryCall<TResponse>>)state).Call.GetStatus();
             internal static readonly Func<object, Metadata> GetTrailers = state => ((ContextState<AsyncUnaryCall<TResponse>>)state).Call.GetTrailers();
             internal static readonly Action<object> Dispose = state => ((ContextState<AsyncUnaryCall<TResponse>>)state).Dispose();
-        }
-
-        private static class ServerStreamingCallbacks<TResponse>
-            where TResponse : class
-        {
-            internal static readonly Func<object, Task<Metadata>> GetResponseHeadersAsync = state => ((ContextState<AsyncServerStreamingCall<TResponse>>)state).Call.ResponseHeadersAsync;
-            internal static readonly Func<object, Status> GetStatus = state => ((ContextState<AsyncServerStreamingCall<TResponse>>)state).Call.GetStatus();
-            internal static readonly Func<object, Metadata> GetTrailers = state => ((ContextState<AsyncServerStreamingCall<TResponse>>)state).Call.GetTrailers();
-            internal static readonly Action<object> Dispose = state => ((ContextState<AsyncServerStreamingCall<TResponse>>)state).Dispose();
-        }
-
-        private static class DuplexStreamingCallbacks<TRequest, TResponse>
-            where TRequest : class
-            where TResponse : class
-        {
-            internal static readonly Func<object, Task<Metadata>> GetResponseHeadersAsync = state => ((ContextState<AsyncDuplexStreamingCall<TRequest, TResponse>>)state).Call.ResponseHeadersAsync;
-            internal static readonly Func<object, Status> GetStatus = state => ((ContextState<AsyncDuplexStreamingCall<TRequest, TResponse>>)state).Call.GetStatus();
-            internal static readonly Func<object, Metadata> GetTrailers = state => ((ContextState<AsyncDuplexStreamingCall<TRequest, TResponse>>)state).Call.GetTrailers();
-            internal static readonly Action<object> Dispose = state => ((ContextState<AsyncDuplexStreamingCall<TRequest, TResponse>>)state).Dispose();
-        }
-
-        private static class ClientStreamingCallbacks<TRequest, TResponse>
-            where TRequest : class
-            where TResponse : class
-        {
-            internal static readonly Func<object, Task<Metadata>> GetResponseHeadersAsync = state => ((ContextState<AsyncClientStreamingCall<TRequest, TResponse>>)state).Call.ResponseHeadersAsync;
-            internal static readonly Func<object, Status> GetStatus = state => ((ContextState<AsyncClientStreamingCall<TRequest, TResponse>>)state).Call.GetStatus();
-            internal static readonly Func<object, Metadata> GetTrailers = state => ((ContextState<AsyncClientStreamingCall<TRequest, TResponse>>)state).Call.GetTrailers();
-            internal static readonly Action<object> Dispose = state => ((ContextState<AsyncClientStreamingCall<TRequest, TResponse>>)state).Dispose();
         }
     }
 }
